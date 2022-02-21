999246a7c7b2b6a3f4df1dd341e553176b3194ee
Frans Gustavson Påsse <frans.gop@hotmail.com>
2022-02-21 16:13:54 +0100

    Added wrapper so I could structure the content better

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 let circ = new CircleType(document.getElementById("circle-text"));
+
+const hamburger = document.querySelector(".hamburger-menu");
+
+hamburger.addEventListener("click", () => {
+  document.body.classList.toggle("nav-open");
+});
